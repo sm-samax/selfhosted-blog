3a0c056f1bb555356b79f75695361dd666602773
Samuel Meszaros <samuel.meszaros01@gmail.com>
2024-09-25 19:41:28 +0200

    Automate Portainer

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#install docker if it's not already installed
+# install docker if it's not already installed
 if ! command -v docker 2>&1 >/dev/null
 then
     sudo apt-get update -y
@@ -15,9 +15,21 @@ then
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
 
-#double check if installation was successful
+# double check if installation was successful
 if ! command -v docker 2>&1 >/dev/null
 then
     echo "docker wasn't installed, aborting..."
     exit 1
 fi
+
+sudo docker volume create portainer_data
+
+# initalize portainer
+sudo docker run -d \
+    -p 8000:8000 \
+    -p 9443:9443 \
+    --name portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:2.21.2

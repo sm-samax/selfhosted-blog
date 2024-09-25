921d227dfb9a995845e75d74f1c3f76a13981419
Samuel Meszaros <samuel.meszaros01@gmail.com>
2024-09-25 19:28:39 +0200

    Automating docker installation

---
+++ install.sh
@@ -0,0 +1,23 @@
+#install docker if it's not already installed
+if ! command -v docker 2>&1 >/dev/null
+then
+    sudo apt-get update -y
+    sudo apt-get install -y ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+    echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+    $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update -y
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+fi
+
+#double check if installation was successful
+if ! command -v docker 2>&1 >/dev/null
+then
+    echo "docker wasn't installed, aborting..."
+    exit 1
+fi
